b5c66044b65e5f5e9ee4c494b05b7f639d899381
Neil <neil@dip.sun.ac.za>
2013-12-06 18:53:11 +0200

    Add very simple test program

---
+++ tests/test_simple_window.py
@@ -0,0 +1,10 @@
+# Display a window for 2 seconds
+# intended to test that we can display a window
+
+import pygame
+import time
+
+pygame.display.init()
+pygame.display.set_mode((800, 600))
+
+time.sleep(2)
