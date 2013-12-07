d23b82e89c756e8cd51aa2abfacd6cc7b2907fae
Neil <neil@dip.sun.ac.za>
2013-12-07 19:09:07 +0200

    Fix error introduced in rebase

--- pygame/__init__.py
+++ pygame/__init__.py
@@ -9,6 +9,6 @@ from pygame.rect import Rect
 from pygame.surface import Surface
 from pygame.constants import QUIT
 from pygame import display, color, surface, time, event, constants, sprite,\
-     mouse, locals, images
+     mouse, locals, image
 # XXX
 from pygame.display import init
