9a145fd5df247d1b018cc8cb66ce89659a39f874
Maciej Fijalkowski <fijall@gmail.com>
2013-12-07 14:23:51 +0200

    Make blitting work (the basics anyway)

---
+++ pygame/color.py
@@ -0,0 +1,11 @@
+
+class Color(object):
+    def __init__(self, *args):
+        if len(args) == 4:
+            r, g, b, a = args
+        else:
+            raise NotImplementedError("implement me")
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
