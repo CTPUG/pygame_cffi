b2418d55564e8ad319eee63a6ec60914bcf68777
Jeremy Thurgood <firxen@gmail.com>
2013-12-07 18:21:09 +0200

    KEYUP and KEYDOWN events.

--- pygame/event.py
+++ pygame/event.py
@@ -2,6 +2,12 @@
 
 from pygame._sdl import sdl, ffi
 
+from pygame.constants import KEYDOWN, KEYUP
+
+
+_USEROBJECT_CHECK1 = 0xDEADBEEF
+_USEROBJECT_CHECK2 = 0xFEEDF00D
+
 
 class Event(object):
     """An event object"""
@@ -9,8 +15,23 @@ class Event(object):
     def __init__(self, sdlevent):
         self._sdlevent = sdlevent
         self.type = self._sdlevent.type
-        # XXX: Implement this
-        self.dict = {}
+
+        if (sdlevent.user.code == _USEROBJECT_CHECK1 and
+                sdlevent.user.data1 == ffi.cast('void *', _USEROBJECT_CHECK2)):
+            raise NotImplementedError("TODO: User-posted events.")
+
+        # TODO: SDL_ACTIVEEVENT
+        if self.type == KEYDOWN:
+            self.unicode = sdlevent.key.keysym.unicode
+            self.key = sdlevent.key.keysym.sym
+            self.mod = sdlevent.key.keysym.mod
+            self.scancode = sdlevent.key.keysym.scancode
+        elif self.type == KEYUP:
+            self.key = sdlevent.key.keysym.sym
+            self.mod = sdlevent.key.keysym.mod
+            self.scancode = sdlevent.key.keysym.scancode
+        else:
+            raise NotImplementedError("TODO: More event types.")
 
 
 def get(event_filter=None):
