cd07fd188862ba3d989f233d65f6d6e2742a4e8c
Neil <neil@dip.sun.ac.za>
2013-12-07 15:34:41 +0200

    Placeholder time object

--- pygame/__init__.py
+++ pygame/__init__.py
@@ -1,6 +1,6 @@
 """ XXX: fish """
 
-__all__ = ['display', 'color', 'surface', 'Color']
+__all__ = ['display', 'color', 'surface', 'Color', 'time']
 
-from pygame import display, color, surface
+from pygame import display, color, surface, time
 from pygame.color import Color
---
+++ pygame/time.py
@@ -0,0 +1,11 @@
+""" The pygame time module
+"""
+
+
+class Clock(object):
+
+   def __init__(self):
+      pass
+
+   def tick(self, fps):
+      pass
