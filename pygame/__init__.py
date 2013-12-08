026755976126a8abe2b3cb5908cafc7857a50946
Neil <neil@dip.sun.ac.za>
2013-12-08 10:39:48 +0200

    Import pkgdata module from pygame

--- pygame/__init__.py
+++ pygame/__init__.py
@@ -2,14 +2,15 @@
 
 __all__ = ['display', 'color', 'surface', 'Color', 'time', 'event',
            'constants', 'sprite', 'Rect', 'Surface', 'QUIT', 'init',
-           'mouse', 'locals', 'image', 'font', 'mixer', 'transform']
+           'mouse', 'locals', 'image', 'font', 'mixer', 'transform',
+           'pkgdata']
 
 from pygame.color import Color
 from pygame.rect import Rect
 from pygame.surface import Surface
 from pygame.constants import QUIT
 from pygame import display, color, surface, time, event, constants, sprite,\
-     mouse, locals, image, transform
+     mouse, locals, image, transform, pkgdata
 # XXX
 from pygame.display import init
 
