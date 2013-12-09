a8af64eb67ddf132026c8877ad8f19885a173cde
Maciej Fijalkowski <fijall@gmail.com>
2013-12-09 13:59:38 +0200

    fix the RGB (thanks armin)

--- pygame/image.py
+++ pygame/image.py
@@ -36,8 +36,8 @@ def fromstring(string, (w, h), format, flipped=False):
     if len(string) != w * h * 3:
         raise ValueError("String length does not equal format and "
                          "resolution size")
-    surf = sdl.SDL_CreateRGBSurface(0, w, h, 24, 0xff << 16, 0xff << 8,
-                                    0xff, 0)
+    surf = sdl.SDL_CreateRGBSurface(0, w, h, 24, 0xff, 0xff << 8,
+                                    0xff << 16, 0)
     if not surf:
         raise SDLError.from_sdl_error()
     with locked(surf):
