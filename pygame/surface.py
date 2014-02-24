01079854b3432342583171c60b1a6f2f4b599467
Rizmari Versfeld <rizmari@praekeltconsulting.com>
2014-02-24 22:05:28 +0200

    fix/complete Surface.get_at, set_at, get_bounding_rect

--- pygame/surface.py
+++ pygame/surface.py
@@ -6,6 +6,16 @@ from pygame.rect import Rect, new_rect, rect_from_obj
 from pygame.color import create_color, uncreate_color
 
 
+if sdl.SDL_BYTEORDER == sdl.SDL_LIL_ENDIAN:
+    BYTE0 = 0
+    BYTE1 = 1
+    BYTE2 = 2
+else:  # BIG_ENDIAN
+    BYTE0 = 2
+    BYTE1 = 1
+    BYTE2 = 0
+
+
 class SubSurfaceData(object):
     def __init__(self, owner, pixeloffset, xoffset, yoffset):
         self.owner = owner
@@ -42,10 +52,13 @@ class Surface(object):
             else:
                 pix = ffi.new("SDL_PixelFormat*")
                 pix.BitsPerPixel = 32
-                pix.Amask = 0
-                pix.Rmask = 0xff0000
-                pix.Gmask = 0xFF00
-                pix.Bmask = 0xFF
+                if flags & sdl.SDL_SRCALPHA:
+                    pix.Amask = 0xFF000000
+                else:
+                    pix.Amask = 0
+                pix.Rmask = 0x00FF0000
+                pix.Gmask = 0x0000FF00
+                pix.Bmask = 0x000000FF
             self._c_surface = sdl.SDL_CreateRGBSurface(flags, w, h,
                                                        pix.BitsPerPixel,
                                                        pix.Rmask,
@@ -137,6 +150,12 @@ class Surface(object):
         surface._c_surface = c_surface
         return surface
 
+    def check_opengl(self):
+        if not self._c_surface:
+            raise SDLError("display Surface quit")
+        if self._c_surface.flags & sdl.SDL_OPENGL:
+            raise SDLError("Cannot call on OPENGL Surfaces")
+
     def get_rect(self, **kwargs):
         r = Rect(0, 0, self._w, self._h)
         if kwargs:
@@ -146,6 +165,7 @@ class Surface(object):
         return r
 
     def set_at(self, pos, color):
+        self.check_opengl()
         x, y = pos
         c_color = create_color(color, self._format)
         with locked(self._c_surface):
@@ -166,13 +186,36 @@ class Surface(object):
             pixels = ffi.cast("uint32_t*", self._c_surface.pixels)
             pixels[y * (self._c_surface.pitch // bpp) + x] = c_color
         else:
-            raise RuntimeError("Unknown pixel format")
+            raise RuntimeError("invalid color depth for surface")
+
+    def get_at(self, pos):
+        self.check_opengl()
+        x, y = pos
+        with locked(self._c_surface):
+            c_color = self._get_at(x, y)
+        return uncreate_color(c_color, self._format)
+
+    def _get_at(self, x, y):
+        bpp = self._format.BytesPerPixel
+        if bpp == 1:
+            pixels = ffi.cast("uint8_t*", self._c_surface.pixels)
+            return pixels[y * self._c_surface.pitch // bpp + x]
+        elif bpp == 2:
+            pixels = ffi.cast("uint16_t*", self._c_surface.pixels)
+            return pixels[y * self._c_surface.pitch // bpp + x]
+        elif bpp == 3:
+            pixels = ffi.cast("uint8_t*", self._c_surface.pixels)
+            pixel = pixels[y * self._c_surface.pitch // bpp + x]
+            return pixel[BYTE0] | pixel[BYTE1] << 8 | pixel[BYTE2] << 16
+        elif bpp == 4:
+            pixels = ffi.cast("uint32_t*", self._c_surface.pixels)
+            return pixels[y * self._c_surface.pitch // bpp + x]
+        else:
+            raise RuntimeError("invalid color depth for surface")
 
     def subsurface(self, rect):
-        if not self._c_surface:
-            raise SDLError("display Surface quit")
-        if self._c_surface.flags & sdl.SDL_OPENGL:
-            raise SDLError("Cannot call on OPENGL Surfaces")
+        self.check_opengl()
+
         rect = rect_from_obj(rect)
         if (rect.x < 0 or rect.x + rect.w > self._c_surface.w or rect.y < 0 or
             rect.y + rect.h > self._c_surface.h):
@@ -244,10 +287,8 @@ class Surface(object):
         return Surface._from_sdl_surface(newsurf)
 
     def get_colorkey(self):
-        if not self._c_surface:
-            raise SDLError("display Surface quit")
-        if self._c_surface.flags & sdl.SDL_OPENGL:
-            raise SDLError("Cannot call on OPENGL Surfaces")
+        self.check_opengl()
+
         if not self._c_surface.flags & sdl.SDL_SRCCOLORKEY:
             return None
         r = ffi.new("uint8_t[1]")
@@ -258,6 +299,9 @@ class Surface(object):
         return (r[0], g[0], b[0], a[0])
 
     def set_alpha(self, value=None, flags=0):
+        """ set_alpha(value, flags=0) -> None
+        set the alpha value for the full Surface image
+        """
         if value is not None:
             value = int(value)
             if value > 255:
@@ -272,29 +316,109 @@ class Surface(object):
             if sdl.SDL_SetAlpha(self._c_surface, flags, value) == -1:
                 raise SDLError.from_sdl_error()
 
-    def get_at(self, pos):
-        x, y = pos
-        with locked(self._c_surface):
-            bpp = self._format.BytesPerPixel
-            if bpp == 1:
-                pixels = ffi.cast("uint8_t*", self._c_surface.pixels)
-                c_color = pixels[y * self._c_surface.pitch // bpp + x]
-            elif bpp == 2:
-                pixels = ffi.cast("uint16_t*", self._c_surface.pixels)
-                c_color = pixels[y * self._c_surface.pitch // bpp + x]
-            elif bpp == 3:
-                pixels = ffi.cast("uint8_t*", self._c_surface.pixels)
-                raise RuntimeError("Not implemented")
-            elif bpp == 4:
-                pixels = ffi.cast("uint32_t*", self._c_surface.pixels)
-                c_color = pixels[y * self._c_surface.pitch // bpp + x]
-            else:
-                raise RuntimeError("Unknown pixel format")
-        return uncreate_color(c_color, self._format)
+    def get_alpha(self):
+        """ get_alpha() -> int_value or None
+        get the current Surface transparency value
+        """
+        self.check_opengl()
+
+        if self._c_surface.flags & sdl.SDL_SRCALPHA:
+            return int(self._c_surface.format.alpha)
+        return None
 
     def get_clip(self):
+        """ get_clip() -> Rect
+        get the current clipping area of the Surface
+        """
         # TODO: Clipping.
         return self.get_rect()
 
+    def get_bounding_rect(self, min_alpha=1):
+        """ get_bounding_rect(min_alpha = 1) -> Rect
+        find the smallest rect containing data
+        """
+        if not self._c_surface:
+            raise SDLError("display Surface quit")
+
+        min_alpha = int(min_alpha)
+        if min_alpha > 255:
+            min_alpha = 255
+        elif min_alpha < 0:
+            min_alpha = 0
+
+        r, g, b, a = (ffi.new('uint8_t *'), ffi.new('uint8_t *'),
+                      ffi.new('uint8_t *'), ffi.new('uint8_t *'))
+        format = self._c_surface.format
+
+        if self._c_surface.flags & sdl.SDL_SRCCOLORKEY:
+            keyr = ffi.new('uint8_t *')
+            keyg = ffi.new('uint8_t *')
+            keyb = ffi.new('uint8_t *')
+            sdl.SDL_GetRGBA(self._c_surface.colorkey,
+                            format, keyr, keyg, keyb, a)
+            keyr, keyg, keyb = keyr[0], keyg[0], keyb[0]
+        else:
+            keyr = keyg = keyb = None
+
+        min_x, min_y, max_x, max_y = 0, 0, self._w, self._h
+
+        def check_alpha(x, y):
+            value = self._get_at(x, y)
+            sdl.SDL_GetRGBA(value, format, r, g, b, a)
+            if (keyr is None and a[0] >= min_alpha) or \
+               (keyr is not None and (r[0] != keyr or
+                                      g[0] != keyg or
+                                      b[0] != keyb)):
+               return True
+            return False
+
+        with locked(self._c_surface):
+            found_alpha = False
+            for y in range(max_y - 1, -1, -1):
+                for x in range(min_x, max_x):
+                    found_alpha = check_alpha(x, y)
+                    if found_alpha:
+                        break
+                if found_alpha:
+                    break
+                max_y = y
+
+            found_alpha = False
+            for x in range(max_x - 1, -1, -1):
+                for y in range(min_y, max_y):
+                    found_alpha = check_alpha(x, y)
+                    if found_alpha:
+                        break
+                if found_alpha:
+                    break
+                max_x = x
+
+            found_alpha = False
+            for y in range(min_y, max_y):
+                min_y = y
+                for x in range(min_x, max_x):
+                    found_alpha = check_alpha(x, y)
+                    if found_alpha:
+                        break
+                if found_alpha:
+                    break
+
+            found_alpha = False
+            for x in range(min_x, max_x):
+                min_x = x
+                for y in range(min_y, max_y):
+                    found_alpha = check_alpha(x, y)
+                    if found_alpha:
+                        break
+                if found_alpha:
+                    break
+
+        return Rect(min_x, min_y, max_x - min_x, max_y - min_y)
+
     def get_flags(self):
+        """ get_flags() -> int
+        get the additional flags used for the Surface
+        """
+        if not self._c_surface:
+            raise SDLError("display Surface quit")
         return self._c_surface.flags
