babe2885a426ab0a634443cf10b905908c3b954f
Jeremy Thurgood <firxen@gmail.com>
2014-10-05 15:22:20 +0200

    Build multi-arch sdlmain_osx with absolute paths.

--- build_sdlmain_osx.sh
+++ build_sdlmain_osx.sh
@@ -4,8 +4,8 @@
 # magic.
 
 echo "Compiling..."
-gcc -c -fpic -I/usr/local/include/SDL pygame/sdlmain_osx.m -o pygame/sdlmain_osx.o
+gcc -arch i386 -arch x86_64 -c -fpic -I/usr/local/include/SDL `pwd`/pygame/sdlmain_osx.m -o `pwd`/pygame/sdlmain_osx.o
 echo "Linking..."
-gcc -lobjc -framework Cocoa -lSDL -shared -o pygame/libsdlmain_osx.so pygame/sdlmain_osx.o
+gcc -arch i386 -arch x86_64 -lobjc -framework Cocoa -lSDL -shared -o `pwd`/pygame/libsdlmain_osx.so `pwd`/pygame/sdlmain_osx.o
 echo "Cleanup..."
 rm -rf pygame/__pycache__/
