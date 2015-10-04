b84b02a6d2e320b631f9731fad7b6bf1db66db4b
Stefano Rivera <stefano@rivera.za.net>
2015-10-04 09:34:15 +0200

    Claim Python 3 support

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
