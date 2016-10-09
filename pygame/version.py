f07c600a538aeca7bdc86c16f730c09749c8a9cf
Neil <neil@dip.sun.ac.za>
2016-10-09 19:52:38 +0200

    Add version module

---
+++ pygame/version.py
@@ -0,0 +1,10 @@
+#
+# We hardcode the version number and beta level to match recent pygame trunk.
+# This needs to be updated manually
+
+ver = '1.9.2b8'
+# We could extract this from version, as pygame does at build time, but that seems
+# overly complicated.
+vernum = (1, 9, 2)
+# This should be updated before releasing
+pygame_cffi_version = '0.1.1'
