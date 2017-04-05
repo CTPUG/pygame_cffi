2e896b9ff8a2d9b24b98ed8cef788e16a50ca973
Neil <neil@dip.sun.ac.za>
2017-04-05 21:54:28 +0200

    Work towards providing a filter test option to the runners

--- conformance/conf_tests/__init__.py
+++ conformance/conf_tests/__init__.py
@@ -16,65 +16,31 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA  02110-1301  USA
 
-from .helpers import gen_test_image, test_conformance
+from .helpers import gen_test_image, test_conformance, conformance_test_case
 
-from .test_lines import (test_horz_line, test_horz_line_width,
-                         test_vert_line, test_vert_line_width,
-                         test_lines, test_lines_width,
-                         test_lines_function)
+import test_lines
+import test_transforms
+import test_shapes
+import test_surface
+import test_blending
+import test_smoothscale
 
-from .test_transforms import (test_flip, test_scale, test_rotate, test_chop,
-                              test_rotozoom, test_flip_subsurface,
-                              test_chop_subsurface)
-from .test_shapes import (test_rect, test_polygon, test_hollow_circles,
-                          test_filled_circles, test_filled_ellipses_1,
-                          test_filled_ellipses_2, test_hollow_ellipses,
-                          test_filled_circles_limits)
-from .test_surface import test_scroll
-from .test_blending import (test_rgba_add, test_rgba_sub, test_rgba_min,
-                            test_rgba_max, test_rgba_mult, test_rgb_mult,
-                            test_blend_mult)
-from .test_smoothscale import (test_int_smoothscale, test_x_smoothscale,
-                               test_y_smoothscale, test_varied_smoothscale,
-                               test_subsurface_smoothscale)
 
-conformance_tests = [
-        test_horz_line,
-        test_horz_line_width,
-        test_vert_line,
-        test_vert_line_width,
-        test_lines,
-        test_lines_width,
-        test_lines_function,
-        test_subsurface_smoothscale,
-        test_varied_smoothscale,
-        test_y_smoothscale,
-        test_x_smoothscale,
-        test_int_smoothscale,
-        test_filled_circles_limits,
-        test_hollow_ellipses,
-        test_filled_ellipses_1,
-        test_filled_ellipses_2,
-        test_filled_circles,
-        test_hollow_circles,
-        test_rect,
-        test_polygon,
-        test_scroll,
-        test_rgba_add,
-        test_rgba_sub,
-        test_rgba_min,
-        test_rgba_max,
-        test_rgba_mult,
-        test_rgb_mult,
-        test_blend_mult,
-        test_rotozoom,
-        test_scale,
-        test_flip,
-        test_flip_subsurface,
-        test_rotate,
-        test_chop,
-        test_chop_subsurface,
-]
+class dummy(object):
+    def __init__(self):
+        self.filter = None
+        self.verbose = True
+
+
+def cmd_args(argv):
+    """Argument parser for the gen_conformannce and test_conformance
+       scripts."""
+    return dummy()
+
+
+def conformance_tests(test_filter):
+    """Return the tests to run"""
+    return conformance_test_case.get_tests()
 
 
 __all__ = [conformance_tests, gen_test_image, test_conformance]
